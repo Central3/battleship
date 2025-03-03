fcef6174e69c8e60b44da76e0cff182d9512abad
Central3 <pawarrsumukha@gmail.com>
2025-03-03 23:26:46 +0530

    Refactor ship placement to handle edge cases with tests

--- src/gameBoard.js
+++ src/gameBoard.js
@@ -1,3 +1,5 @@
+import ship from "./ship";
+
 export default function gameBoard() {
   const boardDimension = 10;
   let board = [...Array(boardDimension)].map((row) =>
@@ -5,25 +7,39 @@ export default function gameBoard() {
   );
 
   function isValidCoordinate(x, y) {
-    return x > 0 && x < boardDimension && y > 0 && y < boardDimension;
+    return x >= 0 && x < boardDimension && y >= 0 && y < boardDimension;
   }
 
   function placeShip(ship, row, col, axis) {
     if (!isValidCoordinate(row, col)) return "Out of bounds";
 
+    if (axis === "x" && col + ship.length > boardDimension)
+      return "Invalid ship placement";
+    if (axis === "y" && row + ship.length > boardDimension)
+      return "Invalid ship placement";
+
+    const horizontalScan = board[row]
+      .slice(col, col + ship.length)
+      .find((cell) => cell !== null);
+
+    const verticalScan = board
+      .map((arr) => arr[col])
+      .slice(row, row + ship.length)
+      .find((cell) => cell !== null);
+
     if (axis === "x") {
-      if (col + ship.length > boardDimension) return "Invalid ship placement";
+      if (horizontalScan !== undefined) return "Overlapping";
       for (let i = 0; i < ship.length; i++) {
-        board[1][col + i] = 1;
+        board[row][col + i] = ship;
       }
     } else {
-      if (row + ship.length > boardDimension) return "Invalid ship placement";
+      if (verticalScan !== undefined) return "Overlapping";
       for (let i = 0; i < ship.length; i++) {
-        board[row + i][col] = 1;
+        board[row + i][col] = ship;
       }
     }
     return board;
   }
 
-  return { board, placeShip };
+  return { board, placeShip, receiveAttack };
 }
--- src/tests/gameBoard.test.js
+++ src/tests/gameBoard.test.js
@@ -2,6 +2,13 @@ import gameBoard from "../gameBoard";
 import ship from "../ship";
 
 let testGameBoard;
+let carrier, battleship, destroyer, submarine, patrolBoat;
+
+beforeAll(() => {
+  testGameBoard = gameBoard();
+  [carrier, battleship, destroyer, submarine, patrolBoat] =
+    testGameBoard.createTestFleet();
+});
 
 beforeEach(() => {
   testGameBoard = gameBoard();
@@ -17,34 +24,52 @@ it("should initialize a 10x10 board with all cells set to null", () => {
 
 describe("Ship placement", () => {
   it("should place a ship horizontally on the board", () => {
-    const carrier = ship(5);
-    testGameBoard.placeShip(carrier, 1, 1, "x");
-    expect(testGameBoard.board[1][1]).toBe(1);
-    expect(testGameBoard.board[1][2]).toBe(1);
-    expect(testGameBoard.board[1][3]).toBe(1);
-    expect(testGameBoard.board[1][4]).toBe(1);
-    expect(testGameBoard.board[1][5]).toBe(1);
+    testGameBoard.placeShip(carrier, 4, 1, "x");
+    expect(testGameBoard.board[4].slice(1, 6)).toEqual([
+      carrier,
+      carrier,
+      carrier,
+      carrier,
+      carrier,
+    ]);
   });
 
   it("should place a ship vertically on the board", () => {
-    const destroyer = ship(3);
     testGameBoard.placeShip(destroyer, 1, 1, "y");
-    expect(testGameBoard.board[1][1]).toBe(1);
-    expect(testGameBoard.board[2][1]).toBe(1);
-    expect(testGameBoard.board[3][1]).toBe(1);
+    expect(testGameBoard.board[1][1]).toBe(destroyer);
+    expect(testGameBoard.board[2][1]).toBe(destroyer);
+    expect(testGameBoard.board[3][1]).toBe(destroyer);
   });
 
   it("should return 'Out of bounds' when placing a ship outside the board", () => {
-    const destroyer = ship(3);
     expect(testGameBoard.placeShip(destroyer, -1, 15, "x")).toEqual(
       "Out of bounds",
     );
   });
 
   it("should return 'Invalid ship placement' when ship placement exceeds board boundaries", () => {
-    const destroyer = ship(3);
-    expect(testGameBoard.placeShip(destroyer, 9, 9, "y")).toEqual(
+    expect(testGameBoard.placeShip(destroyer, 3, 8, "x")).toMatch(
       "Invalid ship placement",
+    );
+  });
+
+  it("should return 'Invalid ship placement' when ship placement exceeds board boundaries", () => {
+    expect(testGameBoard.placeShip(destroyer, 9, 9, "y")).toMatch(
+      "Invalid ship placement",
+    );
+  });
+
+  it("should return 'Overlapping' when ships are placed on top of each other", () => {
+    testGameBoard.placeShip(carrier, 1, 1, "x");
+    expect(testGameBoard.placeShip(destroyer, 0, 1, "y")).toMatch(
+      "Overlapping",
+    );
+  });
+
+  it("should return 'Overlapping' when ships are placed on top of each other", () => {
+    testGameBoard.placeShip(carrier, 0, 1, "y");
+    expect(testGameBoard.placeShip(destroyer, 1, 0, "x")).toMatch(
+      "Overlapping",
     );
   });
 });
