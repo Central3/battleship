418dc4bdcbb55bd36474a0eb7355b4623b83d108
Central3 <pawarrsumukha@gmail.com>
2025-03-19 22:03:21 +0530

    Implement random ship placement

--- src/index.js
+++ src/index.js
@@ -1,36 +1,54 @@
 import "./style.css";
 import { initGame, computerMove } from "./gameController";
 import { renderBoard } from "./UIController";
+import refreshIcon from "./icons/refresh-icon.svg";
 
+const gameContainer = document.querySelector(".game-container");
 const playerBoardDisplay = document.querySelector(".player-game-board");
 const computerBoardDisplay = document.querySelector(".computer-game-board");
 
-const controller = new AbortController();
+let placedFleet = false;
 
 const { human, computer } = initGame();
-let humanBoardData = human.gameBoard;
-let computerBoardData = computer.gameBoard;
+const humanBoardData = human.gameBoard;
+const computerBoardData = computer.gameBoard;
 let activePlayer = human;
 renderBoard(humanBoardData.board, playerBoardDisplay, false);
 renderBoard(computerBoardData.board, computerBoardDisplay, true);
 
-computerBoardDisplay.addEventListener(
-  "click",
-  (event) => {
-    if (event.target.classList.contains("cell") && activePlayer === human) {
-      const { row, col } = event.target.dataset;
-      if (
-        computerBoardData.board[row][col] === 0 ||
-        computerBoardData.board[row][col] === "x"
-      )
-        return;
-      computerBoardData.receiveAttack(row, col);
-      switchActivePlayer();
-      updateScreen();
-    }
-  },
-  { signal: controller.signal },
-);
+const refreshIconElement = new Image();
+refreshIconElement.src = refreshIcon;
+refreshIconElement.classList.add("reposition");
+gameContainer.appendChild(refreshIconElement);
+
+function handleBoardClick(event) {
+  if (
+    event.target.classList.contains("interact") &&
+    activePlayer === human &&
+    placedFleet
+  ) {
+    const { row, col } = event.target.dataset;
+    if (
+      computerBoardData.board[row][col] === 0 ||
+      computerBoardData.board[row][col] === "x"
+    )
+      return;
+    computerBoardData.receiveAttack(row, col);
+    switchActivePlayer();
+    updateScreen();
+  }
+}
+
+refreshIconElement.addEventListener("click", () => {
+  placedFleet = true;
+  humanBoardData.clearBoard();
+  humanBoardData.placeFleet();
+  computerBoardData.clearBoard();
+  computerBoardData.placeFleet();
+
+  computerBoardDisplay.addEventListener("click", handleBoardClick);
+  updateScreen();
+});
 
 function switchActivePlayer() {
   activePlayer = activePlayer === human ? computer : human;
@@ -42,19 +60,19 @@ function updateScreen() {
 
   if (activePlayer === computer) {
     computerMove(humanBoardData);
-    playerBoardDisplay.textContent = "";
-    renderBoard(humanBoardData.board, playerBoardDisplay, false);
     switchActivePlayer();
   }
+  playerBoardDisplay.textContent = "";
+  renderBoard(humanBoardData.board, playerBoardDisplay, false);
 
   if (computerBoardData.isAllShipsSunk() && humanBoardData.isAllShipsSunk()) {
     console.log("Draw!!!");
-    controller.abort();
+    computerBoardDisplay.removeEventListener("click", handleBoardClick);
   } else if (computerBoardData.isAllShipsSunk()) {
     console.log("Player won!!!");
-    controller.abort();
+    computerBoardDisplay.removeEventListener("click", handleBoardClick);
   } else if (humanBoardData.isAllShipsSunk()) {
     console.log("Computer won!!!");
-    controller.abort();
+    computerBoardDisplay.removeEventListener("click", handleBoardClick);
   }
 }
